f3a2cb6965b55f4fe0bd18c78f0227126999b7aa
LucasTemponi <lucastmpn@gmail.com>
2022-04-02 15:22:32 -0300

    Implementa funçãopara buscar artigos do backend

--- src/pages/ArtigosPage/index.tsx
+++ src/pages/ArtigosPage/index.tsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import { ArticleList } from "../../components/ArticleList";
 import { ArticleThumbnailProps } from "../../components/ArticleThumbnail/ArticleThumbnail.types";
+import apiClient from "../../services/api-client";
 import { geraArtigos } from "../../stories/helpers/gerador-artigos";
-
 
 export const ArtigosPage = () => { 
     const [articles, setArticles] = useState<ArticleThumbnailProps[]>([]);
+ 
+    async function buscaArtigos() {
+        // através de generics, posso informar ao axios o tipo de objeto que vamos
+        // operar.
+        const response = await apiClient.get<ArticleThumbnailProps[]>(
+          '/artigos'
+        );
+        setArticles(response.data);
+      }
     
-    useEffect(() => {
-         setArticles(geraArtigos(10));
-    }, []);
+      useEffect(() => {
+        buscaArtigos();
+      }, []);
 
 return (
     <div className="my-30">
