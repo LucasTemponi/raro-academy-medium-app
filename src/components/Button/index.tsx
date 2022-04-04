0340fa078073234a59b5654b6f529d76d2f1b853
LucasTemponi <lucastmpn@gmail.com>
2022-04-04 19:22:11 -0300

    Corrige bug com cores dos botões causado por concatenação de strings nas classes Tailwind

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -17,11 +17,11 @@ export const Button: React.FC<ButtonProps> = ({
   return (
       <button
         type={ type }
-        className={`
-          w-full mt-6 tracking-widest    
-          border-b-${primaryColor}-600 bg-${primaryColor}-500 py-3 text-white font-bold
-          hover:bg-${primaryColor}-400 active:translate-y-[0.125rem] active:border-b-${primaryColor}-400
-        `}
+        className={
+          {'blue':'w-full mt-6 tracking-widest border-b-blue-600 bg-blue-500 py-3 text-white font-bold hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400',
+          'gray':'w-full mt-6 tracking-widest border-b-gray-600 bg-gray-500 py-3 text-white font-bold hover:bg-gray-400 active:translate-y-[0.125rem] active:border-b-gray-400',
+          'red':'w-full mt-6 tracking-widest border-b-red-600 bg-red-500 py-3 text-white font-bold hover:bg-red-400 active:translate-y-[0.125rem] active:border-b-red-400'}        
+          [primaryColor]} //Obrigado TailwindCSS por não me permitir concatenar strings em classes
         onClick={onClick}
       >
         { children }
